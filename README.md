03b4cb8ef53ba0e4838de9f254f396ad65884017
Mohsin Ali <am3375011@gmail.com>
2024-05-18 09:01:09 +0500

    user and video models created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Backend project, creating backend web application similar to youtube.
+
+[Data Model](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import connectDB from "./db/indexDB.js";
 import app from "./app.js";
 dotenv.config({
   path: "./env",
